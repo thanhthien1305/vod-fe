1f0397fe8f4f077ec22db32962f61fd547c31087
Ma Thanh Thiện <100114914+thanhthien1305@users.noreply.github.com>
2025-03-24 08:22:21 +0700

    fix: signin

--- react/app/api/auth/auth.tsx
+++ react/app/api/auth/auth.tsx
@@ -1,6 +1,7 @@
 const cognitoDomain = process.env.NEXT_PUBLIC_COGNITO_DOMAIN;
 const redirect_url = window.location.href; 
 const clientId = process.env.NEXT_PUBLIC_CLIENT_ID; 
+const clientSecret = process.env.NEXT_PUBLIC_CLIENT_SECRET;
 
 export function signin(code?: string) {
     if (!clientId || !cognitoDomain) {
@@ -14,13 +15,14 @@ export function signin(code?: string) {
             redirect_uri: redirect_url
         }).toString();
     
-        window.location.href = `${cognitoDomain}login?${queryString}`;
+        window.location.href = `${cognitoDomain}/login?${queryString}`;
     } else {
         // Xử lý khi có code, ví dụ gọi API để lấy token
         fetch(`${cognitoDomain}/oauth2/token`, {
             method: "POST",
             headers: {
-                "Content-Type": "application/x-www-form-urlencoded"
+                "Content-Type": "application/x-www-form-urlencoded",
+                "Authorization": "Basic MW11cHI2bmdlZ2E1OXRvcDBlZm02bDM3dDZmN2RzbDY2Y3Fua244Nm9sc2c5MWg1aWVkdQ=="
             },
             body: new URLSearchParams({
                 grant_type: "authorization_code",
