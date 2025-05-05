39b864b745e55a027e8ba977951bdf0631d7e6a6
thanhthien1305 <100114914+thanhthien1305@users.noreply.github.com>
2025-05-05 20:33:05 +0700

    feat: add film's title when hover

--- react/components/film-card.tsx
+++ react/components/film-card.tsx
@@ -4,14 +4,18 @@ import React from 'react';
 interface FilmCardProps {
   film: any;
 }
-export default function CardFilm({ film}: FilmCardProps) {
+
+export default function CardFilm({ film }: FilmCardProps) {
   return (
-    <div className="overflow-hidden cursor-pointer">
+    <div className="relative overflow-hidden cursor-pointer group w-fit">
       <img 
         src={film?.thumbNailsUrls} 
         alt="Film Card" 
-        className="transition-transform duration-300 transform hover:scale-125 hover:brightness-110 hover:shadow-lg"
+        className="transition-transform duration-300 transform group-hover:scale-125 group-hover:brightness-110 group-hover:shadow-lg"
       />
+      <div className="absolute text-bold-title2 font-bold bottom-0 left-0 w-full  bg-opacity-60 text-white text-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 p-2 text-sm">
+        {film?.title || 'Tên phim'}
+      </div>
     </div>
   );
 }
