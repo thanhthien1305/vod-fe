b767cd9b2df152d1d52446fca4ea3d869169e70e
Ma Thanh Thiện <100114914+thanhthien1305@users.noreply.github.com>
2025-05-08 09:48:23 +0700

    imp: fix prebuild

--- react/app/auth/page.tsx
+++ react/app/auth/page.tsx
@@ -1,23 +1,27 @@
-// pages/login.js
 "use client";
 import { Button } from '@heroui/button';
-import React, { useEffect } from 'react';
-import { signin, signup } from '../api/auth/auth';
-import { useRouter, useSearchParams } from 'next/navigation';
+import React, { useEffect, useState } from 'react';
+import { signin } from '../api/auth/auth';
 import { loginApp } from '../actions/auth';
-import { Input } from '@heroui/input';
 import { CircularProgress } from '@heroui/react';
+import { useRouter } from 'next/navigation';
 
 const AuthPage = () => {
-  const searchParams = useSearchParams();
-  const code = searchParams.get("code");
+  const [code, setCode] = useState<string | null>(null);
   const router = useRouter();
 
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const codeParam = params.get("code");
+    setCode(codeParam);
+    if (codeParam) {
+      handleLogin(codeParam);
+    }
+  }, []);
+
   const handleLogin = async (code: string) => {
-    if (!code) return;
     try {
       const res = await signin(code)?.then((res) => res.data);
-      console.log(res);
       if (res.access_token) {
         localStorage.setItem("video-on-demand", res.access_token);
         loginApp(res.access_token);
@@ -26,51 +30,48 @@ const AuthPage = () => {
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
-  useEffect(() => {
-    if (code) {
-      handleLogin(code);
-    }
-  }, [code]);
-
-  return <div className="relative h-screen w-screen">
-    <div
-      className="inset-0 bg-no-repeat bg-cover w-full h-full flex justify-center items-center"
-      style={{ backgroundImage: "url('./landing-page/bg-landing-page.png')" }}
-    >
-      <div className="absolute inset-0 layer-landing from-transparent to-black/50 z-10" />
-      {
-        code ? (
-          <>
-            <div className="font-bold text-bold-title1 text-white">
-              Waiting for authentication...
-            </div>
-            <CircularProgress aria-label="Loading..." />
-          </>
-        ) : (
-          <div className="text-center z-20 space-y-8">
-            <div>
+  return (
+    <div className="relative h-screen w-screen">
+      <div
+        className="inset-0 bg-no-repeat bg-cover w-full h-full flex justify-center items-center"
+        style={{ backgroundImage: "url('./landing-page/bg-landing-page.png')" }}
+      >
+        <div className="absolute inset-0 layer-landing from-transparent to-black/50 z-10" />
+        {
+          code ? (
+            <>
               <div className="font-bold text-bold-title1 text-white">
-                Unlimited movies, TV shows, and more
+                Waiting for authentication...
               </div>
-              <div className="text-regular-title2 text-white">Starts at 70,000 ₫. Cancel anytime.</div>
-            </div>
+              <CircularProgress aria-label="Loading..." />
+            </>
+          ) : (
+            <div className="text-center z-20 space-y-8">
+              <div>
+                <div className="font-bold text-bold-title1 text-white">
+                  Unlimited movies, TV shows, and more
+                </div>
+                <div className="text-regular-title2 text-white">
+                  Starts at 70,000 ₫. Cancel anytime.
+                </div>
+              </div>
 
-            <div className="space-y-4 w-[80%] mx-auto">
-              <div className="text-regular-title4 text-white">Ready to watch? Create your membership.</div>
-              <div className="flex gap-3 h-[60px] items-center justify-around">
-
-                <Button onPress={() => signin()} variant="flat" className="rounded-xl bg-red-primary font-medium text-medium-title3 w-1/4">
-                  Get Started
-                </Button>
+              <div className="space-y-4 w-[80%] mx-auto">
+                <div className="text-regular-title4 text-white">Ready to watch? Create your membership.</div>
+                <div className="flex gap-3 h-[60px] items-center justify-around">
+                  <Button onPress={() => signin()} variant="flat" className="rounded-xl bg-red-primary font-medium text-medium-title3 w-1/4">
+                    Get Started
+                  </Button>
+                </div>
               </div>
             </div>
-          </div>
-        )
-      }
+          )
+        }
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default AuthPage;
